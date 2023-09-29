c8cf0cb35d9a67aade77dcae581a25f7d142b8eb
Irina <invase12@gmail.com>
2023-09-29 17:00:39 +0300

    first commit

---
+++ space_str/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  space_str
+//
+//  Created by Irina Alexandrova on 29.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
